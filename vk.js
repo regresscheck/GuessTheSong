1b9e6c13ed043bb8a218cceb3246c94839a6903b
Andrey Putilin <regresscheck@gmail.com>
2015-09-11 14:41:19 +0300

    Added passport.js and initial VK changes

---
+++ vk.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Andrey on 11.09.2015.
+ */
+module.exports = {
+    'clientID': VKONTAKTE_APP_ID, // FILL THIS SHIT
+    'clientSecret': VKONTAKTE_APP_SECRET,
+    'callbackURL': 'http://localhost:3000/auth/vkontakte/callback'
+};
